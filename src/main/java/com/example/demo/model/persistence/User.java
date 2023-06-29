d8b33173e9f513fe296fbbf6b0d9607e9e5e5905
Ashwin Pillai <13049220+pillai-ashwin@users.noreply.github.com>
2023-06-29 18:10:30 -0400

    take username pwd from login request and return jwt in header if successful

--- src/main/java/com/example/demo/model/persistence/User.java
+++ src/main/java/com/example/demo/model/persistence/User.java
@@ -12,9 +12,11 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 
 @Entity
+@Data
 @Table(name = "user")
 public class User {
 
@@ -27,35 +29,12 @@ public class User {
 	@JsonProperty
 	private String username;
 	
+	@JsonProperty(access= JsonProperty.Access.WRITE_ONLY)
+	@Column(nullable = false)
+	private String password;
+	
 	@OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "cart_id", referencedColumnName = "id")
 	@JsonIgnore
     private Cart cart;
-	
-	public Cart getCart() {
-		return cart;
-	}
-
-	public void setCart(Cart cart) {
-		this.cart = cart;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	
-	
 }
